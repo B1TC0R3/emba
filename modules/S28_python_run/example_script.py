d87b5eafb9c9ed5ca769f1e7ff9defc9c7c38178
Thomas Gingele <b1tc0r3@proton.me>
2024-08-09 14:14:16 +0200

    Improve logging

--- modules/S28_python_run/example_script.py
+++ modules/S28_python_run/example_script.py
@@ -14,21 +14,40 @@
 #
 # Description: This python script servers as an example to create others. It only echoes passed parameters and then exits.
 from sys import argv
+from os import environ
 
 
-def main():
-    print("+++ BEGIN PYTHON OUTPUT +++")
-    print(f"[*] Process '{argv[0]}' started.")
-
-    if len(argv) > 1:
-        for arg in argv[1:]:
-            print(f"[*] Received argument: '{arg}'")
-
-    else:
-        print(f"[*] No arguments received.")
-
-    print("[*] Process '{argv[0]}' finished.")
-    print("++++ END PYTHON OUTPUT ++++")
+LOGFILE = None
 
 
-main()
+def example_script(arguments: dict):
+    # This method contains the module code
+    log("Recvied arguments:")
+    for key, value in enumerate(arguments):
+        log(f"\t- {key} :: {value}")
+
+
+def log(text):
+    print(text)
+
+    if LOGFILE:
+        LOGFILE.writelines(text)
+
+
+# Module and logging setup. Not intended to be changed.
+def setup():
+    global LOGFILE
+    logfile_dir = environ.get('LOG_PATH_MODULE')
+    filename = argv[0].split("/")[-1]
+    LOGFILE = open(f"{logfile_dir}/{filename}", "w")
+
+    log(f"[*] Process '{argv[0]}' started.")
+    example_script({"argv": argv})
+
+    if LOGFILE:
+        LOGFILE.close()
+
+    log(f"[*] Process '{filename}' finished.")
+
+
+setup()
